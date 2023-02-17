021d3be6344b3c77dd8665f6024753e118c20d0b
tranduckhuy <duchuy81.1610@gmail.com>
2023-02-17 22:53:16 +0700

    No organized structure

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package GUI;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.text.MessageFormat;
